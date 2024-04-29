d3bb7b0f45f9e895aa65df23662546a2ef314d37
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-04-30 01:25:24 +0900

    Update 1bit_FA.v

--- 1bit_FA.v
+++ 1bit_FA.v
@@ -1,2 +1,8 @@
-module 1bit_FA();
+module 1bit_FA(A, B, C_in, result, C_out);
+	input A,B,C_in;
+	output wire result, C_out;
+
+	assign result = (~A&~B&C_in) | (~A&B&~C_in) | (A&B&C_in) | (A&~B&~C_in); 
+	assign C_out = (A&B) | (B&C_in) | (A&C_in);
+
 endmodule
