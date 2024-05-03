6e74bc6b968a6df94b068f7dfc1f517294635371
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-05-03 09:02:05 +0900

    Update 4bit_RCA.v

--- 4bit_RCA.v
+++ 4bit_RCA.v
@@ -1,4 +1,4 @@
-module 4bit_RCA(
+module bit4_RCA(
     input [3:0] A, B,
     input Cin,
     output [3:0] Sum,
@@ -7,9 +7,9 @@ module 4bit_RCA(
 
     wire Carry1, Carry2, Carry3;
 
-1bit_FA FA0(.A(A[0]), .B(B[0]), .Cin(Cin), .Sum(Sum[0]), .Cout(Carry1));
-1bit_FA FA1(.A(A[1]), .B(B[1]), .Cin(Carry1), .Sum(Sum[1]), .Cout(Carry2));
-1bit_FA FA2(.A(A[2]), .B(B[2]), .Cin(Carry2), .Sum(Sum[2]), .Cout(Carry3));
-1bit_FA FA3(.A(A[3]), .B(B[3]), .Cin(Carry3), .Sum(Sum[3]), .Cout(Cout));
+bit1_FA FA0(.A(A[0]), .B(B[0]), .Cin(Cin), .Sum(Sum[0]), .Cout(Carry1));
+bit1_FA FA1(.A(A[1]), .B(B[1]), .Cin(Carry1), .Sum(Sum[1]), .Cout(Carry2));
+bit1_FA FA2(.A(A[2]), .B(B[2]), .Cin(Carry2), .Sum(Sum[2]), .Cout(Carry3));
+bit1_FA FA3(.A(A[3]), .B(B[3]), .Cin(Carry3), .Sum(Sum[3]), .Cout(Cout));
 
 endmodule
