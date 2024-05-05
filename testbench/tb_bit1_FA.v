bf7d86ed913bc2f66384b6322586a46732a4acc8
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-05-05 13:23:11 +0900

    Update tb_bit1_FA.v

--- testbench/tb_bit1_FA.v
+++ testbench/tb_bit1_FA.v
@@ -3,19 +3,20 @@
 module tb_bit1_FA;
     reg A, B, Cin;
     wire Sum, Cout;
-    
+
+    //전가산기 모듈을 인스턴스 화
     bit1_FA FA(A, B, Cin, Sum, Cout);
 
     initial begin
-        A = 0; B = 0; Cin =0;
+        A = 0; B = 0; Cin =0;    //0 + 0 + 0 = 0
         #10;
-        A = 0; B = 0; Cin =1;
+        A = 0; B = 0; Cin =1;    // 0 + 0 + 1 = 1
         #10;
-        A = 0; B = 1; Cin =0;
+        A = 0; B = 1; Cin =0;    // 0 + 1 + 0 = 1
         #10;
-        A = 1; B = 0; Cin =1;
+        A = 1; B = 0; Cin =1;    // 1 + 0 + 1 = 2
         #10
-        A = 1; B = 1; Cin =1;
+        A = 1; B = 1; Cin =1;    // 1 + 1 + 1 = 3
         #50;
         $finish;
     end
