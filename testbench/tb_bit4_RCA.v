d48a9ee693427937adce46f2071085164da73e09
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-05-05 13:31:13 +0900

    Update tb_bit4_RCA.v

--- testbench/tb_bit4_RCA.v
+++ testbench/tb_bit4_RCA.v
@@ -5,19 +5,20 @@ module tb_bit4_RCA;
     reg Cin;
     wire [3:0] Sum;
     wire Cout;
-    
+
+    4비트 Ripple Carry Adder를 인스턴스화
     bit4_RCA RCA(A, B, Cin, Sum, Cout);
     
     initial begin
-        A = 4'b0000; B = 4'b0000; Cin =0;
+        A = 4'b0000; B = 4'b0000; Cin =0;    // 0 + 0 + 0 = 0
         #10;
-        A = 4'b0010; B = 4'b0001; Cin =0;
+        A = 4'b0010; B = 4'b0001; Cin =0;    // 2 + 1 + 0 = 3
         #10;
-        A = 4'b0010; B = 4'b0001; Cin =1;
+        A = 4'b0010; B = 4'b0001; Cin =1;    // 2 + 1 + 1 = 4
         #10;
-        A = 4'b1111; B = 4'b1111; Cin =0;
+        A = 4'b1111; B = 4'b1111; Cin =0;    // 7 + 7 + 0 = 14
         #10
-        A = 4'b0101; B = 4'b1010; Cin =0;
+        A = 4'b0101; B = 4'b1010; Cin =0;    // 5 + 10 + 0 = 15
         #50;
         $finish;
     end
