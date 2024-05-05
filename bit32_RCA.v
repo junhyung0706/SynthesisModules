3e9dc223feb4dd39d188aeb19d70fd5e73ec546a
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-05-05 13:44:29 +0900

    Update bit32_RCA.v

--- bit32_RCA.v
+++ bit32_RCA.v
@@ -9,7 +9,7 @@ module bit32_RCA(
 
     wire Carry1, Carry2, Carry3, Carry4, Carry5, Carry6, Carry7;
 
-    // 8개의 4비트 RCA모듈을 인스턴스화 
+    // 8개의 4비트 RCA 모듈을 인스턴스화
     bit4_RCA RCA0(A[3:0], B[3:0], Cin, Sum[3:0], Carry1);
     bit4_RCA RCA1(A[7:4], B[7:4], Carry1, Sum[7:4], Carry2);
     bit4_RCA RCA2(A[11:8], B[11:8], Carry2, Sum[11:8], Carry3);
@@ -19,9 +19,8 @@ module bit32_RCA(
     bit4_RCA RCA6(A[27:24], B[27:24], Carry6, Sum[27:24], Carry7);
     bit4_RCA RCA7(A[31:28], B[31:28], Carry7, Sum[31:28], Cout);
 
-    if Cout
-        assign Sum[39:32] = 8'b0000_0000;
-    else
-        assign Sum[39:32] = 8'b0000_0001;
+    // Cout를 기반으로 추가 비트 설정
+    assign Sum[32] = Cout;  // 최종 carry out을 Sum의 추가 비트로 사용
+    assign Sum[39:33] = 7'b000_0000;
 
 endmodule
