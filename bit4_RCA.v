b586d586d24a9731826a61d4c293d911a8763c6c
믿음직한 메추라기 <72088449+junhyung0706@users.noreply.github.com>
2024-05-05 13:27:02 +0900

    Update bit4_RCA.v

--- bit4_RCA.v
+++ bit4_RCA.v
@@ -7,6 +7,7 @@ module bit4_RCA(
 
     wire Carry1, Carry2, Carry3;
 
+    //4개의 Full Adder 모듈을 인스턴스 화
 bit1_FA FA0(.A(A[0]), .B(B[0]), .Cin(Cin), .Sum(Sum[0]), .Cout(Carry1));
 bit1_FA FA1(.A(A[1]), .B(B[1]), .Cin(Carry1), .Sum(Sum[1]), .Cout(Carry2));
 bit1_FA FA2(.A(A[2]), .B(B[2]), .Cin(Carry2), .Sum(Sum[2]), .Cout(Carry3));
